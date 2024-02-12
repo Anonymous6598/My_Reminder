ebb5b965c8e48dd7534bc9fcc1d51bdc8646773d
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2024-02-13 00:03:51 +0300

    Update My_Reminder_List_note_interface.py

--- My_Reminder/My_Reminder_List_note_interface.py
+++ My_Reminder/My_Reminder_List_note_interface.py
@@ -4,8 +4,7 @@ class My_Reminder_List_note_interface(abc.ABC):
     @abc.abstractmethod
     def __delete_note__(self: typing.Self) -> None:
         pass
-
-
+        
     @abc.abstractmethod
     def __add_list__(self: typing.Self) -> None:
         pass  
