2f7667182cdd92034ee9cceebafbf79e31aa197e
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2024-07-05 21:59:14 +0300

    Update My_Reminder_AI.py

--- My_Reminder/My_Reminder_AI.py
+++ My_Reminder/My_Reminder_AI.py
@@ -6,9 +6,9 @@ class My_Reminder_LM(My_Reminder_AI_interface.My_Reminder_AI_interface):
     PROVIDER: None = None # If you know, how to set up local provider, write it in the issue section
     
     def __init__(self: typing.Self) -> None:
-        self.my_diary_ai_client: g4f.local.LocalClient = g4f.local.LocalClient()
+        self.my_reminder_ai_client: g4f.local.LocalClient = g4f.local.LocalClient()
 
     async def __response__(self: typing.Self, prompt: str) -> str:
-        self.response = self.my_diary_ai_client.chat.completions.create(model=self.LM, messages=[{f"role": f"user", f"content": prompt}])
+        self.response = self.my_reminder_ai_client.chat.completions.create(model=self.LM, messages=[{f"role": f"user", f"content": prompt}])
         
         return self.response.choices[0].message.content
