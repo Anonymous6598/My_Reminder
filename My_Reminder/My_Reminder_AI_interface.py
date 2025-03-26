e305da464858c0543cb71d5f9dbe561bc5136c57
Виктор Илић <vikiilitch@outlook.com>
2025-03-26 20:53:55 +0100

    Delete My_Reminder directory

--- My_Reminder/My_Reminder_AI_interface.py
+++
@@ -1,7 +0,0 @@
-import abc, typing
-
-class My_Reminder_AI_interface(abc.ABC):
-    
-    @abc.abstractmethod
-    def __response__(self: typing.Self, prompt: str) -> str:
-        pass
--- My_Reminder/My_Reminder_screen.py
+++
@@ -1,6 +0,0 @@
-import customtkinter, tkinterdnd2, typing
-
-class Window(customtkinter.CTk, TkinterDnD.DnDWrapper):
-    def __init__(self: typing.Self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        customtkinter.CTk.__init__(self, *args, **kwargs)
-        tkinterdnd2.TkinterDnD.TkdndVersion: tkinterdnd2.TkinterDnD._require = tkinterdnd2.TkinterDnD._require(self)
