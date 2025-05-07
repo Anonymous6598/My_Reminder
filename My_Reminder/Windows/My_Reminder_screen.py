de8da9c9530a7071586803e5725ea2892979b009
Виктор Илић <vikiilitch@outlook.com>
2025-05-07 18:52:37 +0200

    Delete My_Reminder directory

--- My_Reminder/Windows/My_Reminder_screen.py
+++
@@ -1,6 +0,0 @@
-import typing, customtkinter, tkinterdnd2 
-
-class Window(customtkinter.CTk, tkinterdnd2.TkinterDnD.DnDWrapper):
-    def __init__(self: typing.Self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        customtkinter.CTk.__init__(self, *args, **kwargs)
-        tkinterdnd2.TkinterDnD.TkdndVersion: tkinterdnd2.TkinterDnD._require = tkinterdnd2.TkinterDnD._require(self)
