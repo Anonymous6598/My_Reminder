6933587452f6d99a863eaefe98209259c699d134
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 19:59:49 +0200

    Update My_Reminder_interface.py

--- My_Reminder/Linux/My_Reminder_interface.py
+++ My_Reminder/Linux/My_Reminder_interface.py
@@ -8,3 +8,11 @@ class My_Reminder_interface(abc.ABC):
     @abc.abstractmethod
     def __create_list_note__(self: typing.Self) -> None:
         pass
+
+    @abc.abstractmethod
+    def __create_list_note__(self: typing.Self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __fullscreen__(self: typing.Self, event: str | None = None) -> None:
+        pass
